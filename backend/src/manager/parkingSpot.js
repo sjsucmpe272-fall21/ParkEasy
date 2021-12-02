1a80a1eebc8be1a963f78a7ed127ae1fe5618283
vivek-joshi26 <vivek.jsh2@gmail.com>
2021-12-02 00:11:34 -0800

    Added end point to get bookings for a user

--- backend/src/manager/parkingSpot.js
+++ backend/src/manager/parkingSpot.js
@@ -1,16 +1,35 @@
 const ParkingSpot = require('../models/parkingSpot');
+const Bookings = require('../models/bookings');
 
 module.exports = {
     GetAllParkingSpotOfUser: function(req) {
         console.log("Inside Manager")
         return ParkingSpot.find({ userId : req.body.userId })
             .exec()
-            .then((papers) => {
-                return papers;
+            .then((spots) => {
+                return spots;
             })
             .catch(error=>{
                 return {
                     error: "No Spots available for this User"
+                };
+            })
+    }
+}
+
+
+
+module.exports = {
+    GetAllBookingsOfUser: function(req) {
+        console.log("Inside Manager")
+        return Bookings.find({ userId : req.params.userId })
+            .exec()
+            .then((bookings) => {
+                return bookings;
+            })
+            .catch(error=>{
+                return {
+                    error: "No bookings available for this User"
                 };
             })
     }
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -5,5 +5,6 @@ var router = express.Router();
 router.post("/register", userController.registerUser);
 router.post("/booking", userController.addBooking);
 router.put("/booking/status", userController.updateBookingStatus);
+router.get("/booking/:userId", userController.getBookingsForUser);
 
 module.exports = router;
